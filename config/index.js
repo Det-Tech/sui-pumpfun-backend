fa79c8d5cb6aa6dd3ec57120b57f62bdc5daf17f
Det-tech <proninkirilll228@gmail.com>
2024-12-22 05:53:48 -0500

    updated package.json

--- config/index.js
+++ config/index.js
@@ -2,4 +2,6 @@ module.exports = {
     mongoURI: "mongodb://test:4L9FWr1buHIYaAst@cluster0-shard-00-00.qwixr.mongodb.net:27017,cluster0-shard-00-01.qwixr.mongodb.net:27017,cluster0-shard-00-02.qwixr.mongodb.net:27017/technoking-hackathon?ssl=true&replicaSet=atlas-wu5x60-shard-0&authSource=admin&retryWrites=true&w=majority",
     JWT_SECRET: "sui pumpfun",
     RPC_URL: "https://devnet.helius-rpc.com/?api-key=ef69ee26-e88c-45e4-8b4b-e232767bc3f1",
+    ADMIN_PKEY: "suiprivkey1qrcy97vn6u5qhreqneq6cz3cxghk5khx44kzs5q9eq8c0a8y9lwgzw8qsm9",
+    BONDING_CONTRACT: ""
 };
